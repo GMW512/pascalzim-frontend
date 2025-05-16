0251678e13c9e4d0125dae2f9632260668946d3b
unknown <nicolasmendes512@gmail.com>
2025-05-16 12:26:28 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+async function executarCodigo() {
+    const codigo = document.getElementById('codigo').value;
+    const resposta = await fetch('https://seu-backend-na-render.onrender.com/compilar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ codigo })
+    });
+  
+    const resultado = await resposta.text();
+    document.getElementById('saida').textContent = resultado;
+  }
+  
